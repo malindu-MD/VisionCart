e0f762a9b4cdacb6ec2760a4a462c87f02026808
malindu-MD <it21173790@my.sliit.lk>
2023-09-07 11:46:51 +0530

    initial commit

---
+++ app/src/main/java/com/example/visioncart/Config.java
@@ -0,0 +1,9 @@
+package com.example.visioncart;
+
+public class Config {
+
+    public static String EMAIL ="your-gmail-address";  //gmail address
+    public static String PASSWORD ="your-password"; //password
+
+
+}
