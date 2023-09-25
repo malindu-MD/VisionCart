6fcec0ae1fc1d199d7cd82f34f79e1195e8e3896
malindu-MD <it21173790@my.sliit.lk>
2023-09-25 14:43:21 +0530

    change

--- app/src/main/java/com/example/visioncart/Config.java
+++
@@ -1,9 +0,0 @@
-package com.example.visioncart;
-
-public class Config {
-
-    public static String EMAIL ="your-gmail-address";  //gmail address
-    public static String PASSWORD ="your-password"; //password
-
-
-}
--- app/src/main/java/com/example/visioncart/validation/test.java
+++
@@ -1,4 +0,0 @@
-package com.example.visioncart.validation;
-
-public class test {
-}
