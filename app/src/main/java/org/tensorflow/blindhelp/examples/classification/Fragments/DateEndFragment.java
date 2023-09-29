06d9d2cc145c9f39e776e53eccf676cb9bea21a4
Bavantha-Senanayake <bavanthasenanayake@gmail.com>
2023-09-29 13:40:33 +0530

    unversioned files

---
+++ app/src/main/java/org/tensorflow/blindhelp/examples/classification/Fragments/DateEndFragment.java
@@ -0,0 +1,27 @@
+package org.tensorflow.blindhelp.examples.classification.Fragments;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import java.util.Calendar;
+
+public class DateEndFragment extends DialogFragment {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        Calendar c=Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+        // Set the minimum date to the current date
+        DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(), (DatePickerDialog.OnDateSetListener) getActivity(), year, month, day);
+        datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis() - 1000); // -1000 to exclude the current date
+        return datePickerDialog;
+    }
+}
